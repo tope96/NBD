2d4be510dd29ae3d6a56977820b63190feb1cfa3
tope96 <pietrzaktomek@hotmail.com>
2020-03-11 22:04:37 +0100

    exercise 8 from lesson 1

---
+++ src/main/scala/cw1/ex8.scala
@@ -0,0 +1,18 @@
+package cw1
+
+object ex8 {
+  def main(args: Array[String]):Unit={
+    val list:List[Int] = List(0, 1, 2, 0, 3, 2, 0, 0, 4, 0)
+    print(removeZ(list, 0))
+  }
+
+  def removeZ(list: List[Int], n: Int): List[Int] = list match {
+    case Nil => Nil
+    case h :: t =>
+    if (h == n)
+      removeZ(t, n)
+    else
+    println(h)
+      h :: removeZ(t, n)
+  }
+}
